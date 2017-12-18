5a2bc19ea22a57cb017bbc8ace6ed3d456469019
Antek Drzewiecki <you.imagine@gmail.com>
2017-12-18 15:56:24 +0100

    Fixed where the callback url didn't want to authenticate. Issue in omniauth-oauth2 gem

--- lib/omniauth/strategies/doorkeeper.rb
+++ lib/omniauth/strategies/doorkeeper.rb
@@ -19,7 +19,16 @@ module OmniAuth
       end
 
       def raw_info
+        puts access_token.inspect
         @raw_info ||= access_token.get('/api/v1/users/me.json').parsed
+      end
+
+
+      # MonkeyPatch for https://github.com/omniauth/omniauth-oauth2/issues/81
+      # Bug as described in https://github.com/doorkeeper-gem/doorkeeper/issues/732
+      #
+      def callback_url
+        full_host + script_name + callback_path
       end
     end
   end
