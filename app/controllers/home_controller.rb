8d31db374b8dcdf5d357aed8d555f193187cf6d1
Antek Drzewiecki <you.imagine@gmail.com>
2017-12-18 17:53:01 +0100

    Logs out the user if the token is invalid. Tempoarary workaround since the error cant be parsed well.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,11 @@ class HomeController < ApplicationController
   end
 
   def posts
+    begin
       @raw_posts = doorkeeper_access_token.get('/api/v1/posts/?format=json').parsed
+    rescue  OAuth2::Error => e
+      redirect_to destroy_user_session_path if e.code == "Not authorized"
+    end
+
   end
 end
