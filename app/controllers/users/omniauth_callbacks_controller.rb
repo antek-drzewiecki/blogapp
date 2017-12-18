9a9fc79d19da02a30f702a1577ee4b9a8d20270f
Antek Drzewiecki <you.imagine@gmail.com>
2017-12-18 15:57:12 +0100

    Refactored the oauth2 authentication to be more simple

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,10 +6,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def doorkeeper
     data = request.env["omniauth.auth"]
     @user = User.from_omniauth(data)
-    @user.update_doorkeeper_credentials(data)
-    @user.save
-
-    sign_in_and_redirect @user, event: :authentication
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      redirect_to root_url
+    end
   end
 
   def failure
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :omniauthable
+  devise :omniauthable, omniauth_providers: [:doorkeeper]
 
-  def self.from_omniauth(oauth_data)
-    User.find_or_initialize_by_doorkeeper_uid(oauth_data.uid).tap do |user|
-      user.email = oauth_data.info.email
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
     end
+
   end
 
   def update_doorkeeper_credentials(oauth_data)
     self.doorkeeper_access_token = oauth_data.credentials.token
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.doorkeeper_data"] && session["devise.doorkeeper_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
 
 end
